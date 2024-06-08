65d6b941720fbb90193177a8d18fa8f478933e8a
minhnndev <ngocminhit2000@gmail.com>
2024-06-08 14:24:01 +0700

    type redux dispatch

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,4 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { useDispatch } from "react-redux";
 
 import rootReducer from "./slice";
 
@@ -8,5 +9,6 @@ const store = configureStore({
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+export const useAppDispatch = useDispatch.withTypes<AppDispatch>();
 
 export default store;
