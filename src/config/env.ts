07d5cecb779ae89b87f7d6d7ce00d81a0bb04ca2
minhnndev <ngocminhit2000@gmail.com>
2025-10-31 02:23:10 +0700

    fix(log): add log check env

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,2 +1,5 @@
+export const ENV = import.meta.env.VITE_ENV;
 export const API_URL = import.meta.env.VITE_API_URL;
 export const SOCKET_URL = import.meta.env.VITE_SOCKET_URL;
+
+console.log(`🚀 ${ENV}:`, API_URL);
