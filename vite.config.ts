2f52391bdb6281434b6850b666038eb8c1b98f06
minhnndev <ngocminhit2000@gmail.com>
2025-10-31 02:48:04 +0700

    fix(frontend): fix vite inject env in runtime instead of build-time

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { defineConfig } from "vite";
+import runtimeEnv from "vite-plugin-runtime-env";
 
 export default defineConfig({
-    plugins: [react()],
+    plugins: [
+        react(),
+        runtimeEnv({
+            variableName: "window.env",
+        }),
+    ],
     resolve: {
         alias: {
             "@": path.resolve(__dirname, "./src"),
